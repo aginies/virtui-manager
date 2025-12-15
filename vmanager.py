c227040edf5b07188aa9b7fc4192730d019db1fc
aginies <aginies@suse.com>
2025-12-15 10:31:45 +0100

    reorganise button order

--- vmanager.py
+++ vmanager.py
@@ -229,8 +229,8 @@ class VMManagerTUI(App):
         yield Header()
         with Horizontal(classes="top-controls"):
             yield Button("Select Servers", id="select_server_button", classes="Buttonpage")
-            yield Button("Server Pref", id="server_preferences_button", classes="Buttonpage")
             yield Button("Servers List", id="manage_servers_button", classes="Buttonpage")
+            yield Button("Server Pref", id="server_preferences_button", classes="Buttonpage")
             yield Button("Filter VM", id="filter_button", classes="Buttonpage")
             yield Button("View Log", id="view_log_button", classes="Buttonpage")
             yield Button("Virsh Shell", id="virsh_shell_button", classes="Buttonpage")
