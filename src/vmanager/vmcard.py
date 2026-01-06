f00609c3d6e15fdb395373a2e241b202f35623e7
aginies <aginies@suse.com>
2026-01-06 11:11:30 +0100

    remove checking local websockify and novnc to diaply Web console button

--- src/vmanager/vmcard.py
+++ src/vmanager/vmcard.py
@@ -595,7 +595,7 @@ class VMCard(Static):
         logging.info(f"graphics_type: {self.graphics_type}")
         # TOFIX
         #self.ui[ButtonIds.WEB_CONSOLE].display = (is_running or is_paused) and self.graphics_type == "vnc" and self.app.websockify_available and self.app.novnc_available
-        self.ui[ButtonIds.WEB_CONSOLE].display = (is_running or is_paused) and self.app.websockify_available and self.app.novnc_available
+        self.ui[ButtonIds.WEB_CONSOLE].display = (is_running or is_paused)# and self.app.websockify_available and self.app.novnc_available
         self.ui[ButtonIds.SNAPSHOT_RESTORE].display = has_snapshots
         self.ui[ButtonIds.SNAPSHOT_DELETE].display = has_snapshots
         self.ui[ButtonIds.CONFIGURE_BUTTON].display = True
