a349c57a8958ae723125d16d38ca2f75c376521f
aginies <aginies@suse.com>
2026-01-06 16:01:38 +0100

    Revert "fix issue displaying VM with same name from different server"
    This reverts commit d31fac23d4a5ad613eeb166808cf20e62139ea60.

--- src/vmanager/vmanager.py
+++ src/vmanager/vmanager.py
@@ -583,7 +583,7 @@ class VMManagerTUI(App):
                 )
 
         self.worker_manager.run(
-            get_details_and_show_modal, name=f"get_details_{message.vm_uuid}"
+            get_details_and_show_modal, name=f"get_details_{message.vm_name}"
         )
 
     @on(VmActionRequest)
