fa5c5fd2c33dec32a7985687fc663b75acc148f4
aginies <aginies@suse.com>
2026-01-23 10:45:50 +0100

    fix build for old python (setup.cfg file)

--- src/vmanager/constants.py
+++ src/vmanager/constants.py
@@ -39,7 +39,7 @@ class AppInfo:
     """Define app data"""
     name = "virtui-manager"
     namecase = "VirtUI Manager"
-    version = "1.0.5"
+    version = "1.0.7"
 
 class VmAction:
     """Defines constants for VM action types."""
