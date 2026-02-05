b323348beeaf16e3a92cb241594bcb6be4987882
aginies <aginies@suse.com>
2026-02-05 17:42:00 +0100

    now developing version 1.6.0

--- src/vmanager/constants.py
+++ src/vmanager/constants.py
@@ -38,7 +38,7 @@ class AppInfo:
     """Define app data"""
     name = "virtui-manager"
     namecase = "VirtUI Manager"
-    version = "1.5.0"
+    version = "1.6.0"
 
 class VmAction:
     """Defines constants for VM action types."""
