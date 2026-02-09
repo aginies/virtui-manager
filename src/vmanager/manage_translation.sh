6fe15cbb4fb56b5d51575ae4d1857519ff8003d7
aginies <aginies@suse.com>
2026-02-09 17:34:05 +0100

    get version from constants.py file

--- src/vmanager/manage_translation.sh
+++ src/vmanager/manage_translation.sh
@@ -10,7 +10,9 @@ LOCALES_DIR="locale"
 DOMAIN="virtui-manager"
 POT_FILE="$LOCALES_DIR/$DOMAIN.pot"
 PACKAGE_NAME="VirtUI Manager"
-PACKAGE_VERSION="1.3.0"
+PACKAGE_VERSION=""
+extract_version
+
 
 function extract_version() {
     # Try to grep version from constants.py
