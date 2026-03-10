b583daa2200254bccf1e295f4a5d5c4667088e2f
aginies <aginies@suse.com>
2026-03-10 16:10:08 +0100

    update

--- src/vmanager/provisioning/providers/__init__.py
+++ src/vmanager/provisioning/providers/__init__.py
@@ -1,3 +1,16 @@
 """
 Provider implementations for different operating systems.
 """
+
+from .debian_provider import DebianProvider, DebianDistro
+from .opensuse_provider import OpenSUSEProvider, OpenSUSEDistro
+from .ubuntu_provider import UbuntuProvider, UbuntuDistro
+
+__all__ = [
+    "DebianProvider",
+    "DebianDistro",
+    "OpenSUSEProvider",
+    "OpenSUSEDistro",
+    "UbuntuProvider",
+    "UbuntuDistro",
+]
