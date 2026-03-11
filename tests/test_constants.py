ed5774e4b12c4ffcd0adc03886ea804168f52848
aginies <aginies@suse.com>
2026-03-12 00:33:49 +0100

    update version

--- tests/test_constants.py
+++ tests/test_constants.py
@@ -13,7 +13,7 @@ class TestConstants(unittest.TestCase):
         """Test AppInfo constants."""
         self.assertEqual(AppInfo.name, "virtui-manager")
         self.assertEqual(AppInfo.namecase, "VirtUI Manager")
-        self.assertEqual(AppInfo.version, "2.3.1")
+        self.assertEqual(AppInfo.version, "2.3.2")
 
     def test_vm_action(self):
         """Test VmAction constants."""
