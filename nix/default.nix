a2c457913d27dcf9734c2fabd3b8bec256dd5806
aginies <aginies@suse.com>
2026-04-13 10:48:15 +0200

    bump release

--- nix/default.nix
+++ nix/default.nix
@@ -2,7 +2,7 @@
 
 pkgs.python3Packages.buildPythonApplication {
   pname = "virtui-manager";
-  version = "2.5.2";
+  version = "2.5.3";
 
   src = ../.;
 
--- src/vmanager/constants.py
+++ src/vmanager/constants.py
@@ -47,7 +47,7 @@ class AppInfo:  # pylint: disable=too-few-public-methods
 
     name = "virtui-manager"
     namecase = "VirtUI Manager"
-    version = "2.5.2"
+    version = "2.5.3"
     author = "Antoine Ginies"
 
 
